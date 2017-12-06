f5a56eabe6adfffff89d294cbf4b5649600746c4
yangzhitong <yangzhitong1@gmail.com>
2017-12-06 23:40:03 +0800

    1"

--- static-assets/index.js
+++ static-assets/index.js
@@ -1,11 +1,11 @@
 let http = require("http"),
   url = require("url"),
   path = require("path"),
-  fs = require("fs");
-
+  fs = require("fs"),
+  zlib = require("zlib");
 let staticPath = "./public/";
 
-let types = {
+let MIME = {
   "txt": "text/plain",
   "xml": "text/xml",
   "html": "text/html",
@@ -26,14 +26,15 @@ let types = {
   "wmv": "video/x-ms-wmv"
 };
 
-let app = http.createServer((request, response) => {
-  let pathName = url.parse(request.url).pathname,
-    realPath = path.join(staticPath, pathName); // 请求文件的在磁盘中的真实地址
-  if (pathName === '/favicon.ico') return;
+let Expires = {
+  fileMatch: /.*/ig,
+  maxAge: 60*60*24 
+};
 
-  let extName = path.extname(realPath);
-  extName = extName ? extName.slice(1) : "";
-  const type = types[extName];
+let app = http.createServer((request, response) => {
+  let pathName = url.parse(request.url).pathname || "",
+    realPath = path.join(staticPath, path.normalize(pathName.replace(/\.\./g, "")));; // 请求文件的在磁盘中的真实地址
+
   fs.exists(realPath, (exists) => {
     if (!exists) {
       // 当文件不存在时
@@ -44,26 +45,47 @@ let app = http.createServer((request, response) => {
       response.write("This request URL ' " + realPath + " ' was not found on this server.");
       response.end();
     } else {
-      // 当文件存在时
-      fs.readFile(realPath, "binary", (err, file) => {
-        if (err) {
-          // 文件读取出错
-          response.writeHead(500, {
-            "Content-Type": "text/plain"
-          });
+      let extName = path.extname(realPath);
+      extName = extName ? extName.slice(1) : "";
+      let contentType = MIME[extName] || "text/plain";
+      response.setHeader('Content-type', `${contentType}; charset=utf-8`);
 
-          response.end(err);
-        } else {
-          // 当文件可被读取时，输出文本流
-          response.writeHead(200, {
-            "Content-Type": "images/jpeg"
-          });
-          response.write(file, "binary");
-          response.end();
-        }
-      });
+      let expires = new Date();
+      expires.setTime(expires.getTime() + Expires.maxAge * 1000);
+      response.setHeader("Expires", expires.toUTCString());
+      response.setHeader("Cache-Control", "max-age=" + Expires.maxAge);
+
+      let stat = fs.statSync(realPath);
+      let lastModified = stat.mtime.toUTCString();
+      response.setHeader("Last-Modified", lastModified);
+      console.log('modify?');
+      if (request.headers["if-modified-since"] && lastModified == request.headers["if-modified-since"]) {
+        response.writeHead(304, "from disk cache");
+        response.end();
+        return;
+      }
+      console.log('modify');
+      let raw = fs.createReadStream(realPath);
+      let acceptEncoding = request.headers['accept-encoding'] || '';
+      if (acceptEncoding.match(/\bdeflate\b/)) {
+        // response.writeHead(200, {
+        //   'Content-Encoding': 'deflate'
+        // });
+        // raw.pipe(zlib.createDeflate()).pipe(response);
+        response.writeHead(200, 'ok');
+        raw.pipe(response);
+      } else if (acceptEncoding.match(/\bgzip\b/)) {
+        console.log(2);
+        response.writeHead(200, {
+          'Content-Encoding': 'gzip'
+        });
+        raw.pipe(zlib.createGzip()).pipe(response);
+      } else {
+        console.log(3);
+        response.writeHead(200, {});
+        raw.pipe(response);
+      }
     }
-
   });
 });
 
