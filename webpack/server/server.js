0b57ee5e1aa7db88f27549b3fd5eb51312a2d073
杨智彤 <747327204@qq.com>
2018-01-11 14:38:34 +0800

    no message

--- webpack/server/server.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import { hello } from "./hello";
-
-export function render() {
-  // 把根组件渲染成 HTML 字符串
-  return renderToString(<hello />);
-}
